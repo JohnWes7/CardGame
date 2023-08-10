5f7139e11bc17210d37bad7d7d74a8fa0e59ad6d
JohnWes7 <1246215651@qq.com>
2023-08-10 14:10:26 +0800

    初始场景

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 玩家数值
+    [SerializeField] private int HP;
+    [SerializeField] private int cost;
+
+
+    // 玩家卡牌
+    [SerializeField] private List<Card> mCards;
+
+    // buff
+
+
+}
---
+++ Assets/Scripts/Model/Card.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card
+{
+    // π¶ƒ‹
+    [SerializeField] private int cost;
+
+    // Õ‚√≤
+    [SerializeField] private Sprite back;
+}
